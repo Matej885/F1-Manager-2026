1e73bc5e19a9b9a83ecd706baf2a1d50a64a75d8
matej <matej@DESKTOP-DVVPRKJ>
2026-03-08 10:34:24 +0100

    Add project files.

---
+++ F1 Manager 2026/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace F1_Manager_2026
+{
+    /// <summary>
+    /// Toto okno slúží iba ako úvodná obrazovka, ktorá sa zobrazí pri spustení hry. 
+    /// Spustí sa F1 Intro a po jeho skončení sa automaticky načíta hlavné menu hry.
+    /// Video sa dá preskočiť kliknutím na tláčítko "Skip Button", ktoré je v pravo dole
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loading_Screen_Intro_Media_Element.Play();
+        }
+        private void MediaElement_MediaFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+            // Keby sa niecčo pokazilo nech máme informáciu o tom čo sa stalo
+            MessageBox.Show(e.ErrorException.Message);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Loading_Screen_Intro_Media_Element.Stop();
+            Options options = new Options();
+            options.Show();
+            this.Close();
+        }
+    }
+}
---
+++ F1 Manager 2026/Options.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Media;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace F1_Manager_2026
+{
+    /// <summary>
+    /// Hlavné nastavenia hry, hráč si tu nastaví preferencie kým sa dostane do herného menu
+    /// 
+    /// </summary>
+    public partial class Options : Window
+    {
+        public Options()
+        {
+            InitializeComponent();
+            SoundPlayer soundPlayer = new SoundPlayer("Sounds/Options_Soundtrack.wav");
+            soundPlayer.Play();
+        }
+        private void MediaRepeat(object sender, RoutedEventArgs e) //aby sa video po skončení spustilo znova
+        {
+            // 1. Vráti video na začiatok
+            options_Media_Element.Position = TimeSpan.FromSeconds(0);
+
+            // 2. Znova ho spustí
+            options_Media_Element.Play();
+        }
+
+        private void Continue_Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Keď bude load system tak ti to načíta hru basically");
+        }
+    }
+
+}
