cda5cf6bf081d36669d1956179aaa87a42e8f194
matej <matej@DESKTOP-DVVPRKJ>
2026-03-25 07:30:22 +0100

    Driver Pick main cast hotova

--- F1 Manager 2026/Drivers.cs
+++ F1 Manager 2026/Drivers.cs
@@ -5,62 +5,71 @@ namespace F1_Manager_2026
 {
     public class Drivers
     {
-        private string _team;
-        public string Name { get; set; }
+        // Pridaný otáznik (string?), aby kompilátor nehlásil chybu "Non-nullable field"
+        private string? _team;
+        public string? Name { get; set; }
         public int Number { get; set; }
-        public string PhotoPath { get; set; }
-        public string SuitPath { get; private set; } // Nastavuje sa automaticky cez Team
+        public string? PhotoPath { get; set; }
+        public string? SuitPath { get; private set; }
         public int Skill { get; set; }
 
-        public string Team
+        public string? Team
         {
-            get => _team;
+            get { return _team; }
             set
             {
                 _team = value;
-                UpdateSuitPath(); // Automatická zmena kombinézy pri zmene tímu
+                UpdateSuitPath();
             }
         }
 
-        public string FormattedCost => $"COST: ${((Skill - 60) * 0.8):F1}M";
+        public string FormattedCost
+        {
+            get
+            {
+                double cost = (Skill - 60) * 0.8;
+                return "COST: $" + cost.ToString("F1") + "M";
+            }
+        }
 
-        // Logika priradenia kombinézy podľa názvu tímu
         private void UpdateSuitPath()
         {
             string folder = "/Images/";
-            SuitPath = _team switch
+            if (_team == null) { SuitPath = folder + "suit_default.png"; return; }
+
+            switch (_team)
             {
-                "McLaren" => folder + "suit_mclaren.png",
-                "Ferrari" => folder + "suit_ferrari.png",
-                "Red Bull Racing" => folder + "RB_suit.png",
-                "Mercedes" => folder + "suit_mercedes.png",
-                "Aston Martin" => folder + "suit_astonmartin.png",
-                "Alpine" => folder + "suit_alpine.png",
-                "Haas" => folder + "suit_haas.png",
-                "RB" => folder + "suit_rb.png",
-                "Williams" => folder + "suit_williams1.png",
-                "Cadillac" => folder + "suit_cadillac.png",
-                "Audi" => folder + "suit_audi.png",
-                "ART Grand Prix" => folder + "suit_art.png",
-                "Invicta Racing" => folder + "suit_invicta.png",
-                "MP Motorsport" => folder + "suit_mp.png",
-                "Campos Racing" => folder + "suit_campos.png",
-                "Rodin Motorsport" => folder + "suit_rodin.png",
-                "DAMS Lucas Oil" => folder + "suit_dams.png",
-                "Trident" => folder + "suit_trident.png",
-                "Hitech" => folder + "suit_hitech.png",
-                "Prema" => folder + "suit_prema.png",
-                "Van Amersfoort Racing" => folder + "suit_var.png",
-                "AIX Racing" => folder + "suit_aix.png",
-                _ => folder + "suit_default.png"
-            };
+                case "McLaren": SuitPath = folder + "suit_mclaren.png"; break;
+                case "Ferrari": SuitPath = folder + "suit_ferrari.png"; break;
+                case "Red Bull Racing": SuitPath = folder + "RB_suit.png"; break;
+                case "Mercedes": SuitPath = folder + "suit_mercedes.png"; break;
+                case "Aston Martin": SuitPath = folder + "suit_astonmartin.png"; break;
+                case "Alpine": SuitPath = folder + "suit_alpine.png"; break;
+                case "Haas": SuitPath = folder + "suit_haas.png"; break;
+                case "RB": SuitPath = folder + "suit_RBJ.png"; break;
+                case "Williams": SuitPath = folder + "suit_williams1.png"; break;
+                case "Cadillac": SuitPath = folder + "suit_cadillac.png"; break;
+                case "Audi": SuitPath = folder + "suit_audi.png"; break;
+                case "ART Grand Prix": SuitPath = folder + "suit_art.png"; break;
+                case "Invicta Racing": SuitPath = folder + "suit_invicta.png"; break;
+                case "MP Motorsport": SuitPath = folder + "suit_mp.png"; break;
+                case "Campos Racing": SuitPath = folder + "suit_campos.png"; break;
+                case "Rodin Motorsport": SuitPath = folder + "suit_rodin.png"; break;
+                case "DAMS Lucas Oil": SuitPath = folder + "suit_dams.png"; break;
+                case "Trident": SuitPath = folder + "suit_trident.png"; break;
+                case "Hitech": SuitPath = folder + "suit_hitech.png"; break;
+                case "Prema": SuitPath = folder + "suit_prema.png"; break;
+                case "Van Amersfoort Racing": SuitPath = folder + "suit_var.png"; break;
+                case "AIX Racing": SuitPath = folder + "suit_aix.png"; break;
+                default: SuitPath = folder + "suit_default.png"; break;
+            }
         }
 
-        // --- F1 DRIVERS ---
+        // VŠETCI JAZDCI DEFINOVANÍ AKO PUBLIC STATIC
         public static Drivers Lando_Norris = new Drivers { Name = "Lando Norris", Number = 4, PhotoPath = "/Images/norris.png", Skill = 95, Team = "McLaren" };
         public static Drivers Oscar_Piastri = new Drivers { Name = "Oscar Piastri", Number = 81, PhotoPath = "/Images/piastri.png", Skill = 92, Team = "McLaren" };
         public static Drivers Charles_Leclerc = new Drivers { Name = "Charles Leclerc", Number = 16, PhotoPath = "/Images/leclerc.png", Skill = 94, Team = "Ferrari" };
-        public static Drivers Lewis_Hamilton = new Drivers { Name = "Lewis Hamilton", Number = 44, PhotoPath = "/Images/hamilton.png", Skill = 93, Team = "Ferrari" };
+        public static Drivers Lewis_Hamilton = new Drivers { Name = "Lewis Hamilton", Number = 44, PhotoPath = "/Images/hamilton.png", Skill = 93, Team = "Mercedes" };
         public static Drivers Max_Verstappen = new Drivers { Name = "Max Verstappen", Number = 33, PhotoPath = "/Images/verstappen.png", Skill = 96, Team = "Red Bull Racing" };
         public static Drivers Isack_Hadjar = new Drivers { Name = "Isack Hadjar", Number = 6, PhotoPath = "/Images/hadjar.png", Skill = 80, Team = "Red Bull Racing" };
         public static Drivers George_Russell = new Drivers { Name = "George Russell", Number = 63, PhotoPath = "/Images/russell.png", Skill = 90, Team = "Mercedes" };
@@ -80,45 +89,28 @@ namespace F1_Manager_2026
         public static Drivers Nico_Hulkenberg = new Drivers { Name = "Nico Hulkenberg", Number = 27, PhotoPath = "/Images/hulkenberg.png", Skill = 84, Team = "Audi" };
         public static Drivers Gabriel_Bortoleto = new Drivers { Name = "Gabriel Bortoleto", Number = 5, PhotoPath = "/Images/bortoleto.png", Skill = 80, Team = "Audi" };
 
-        // --- F2 DRIVERS (Všetci majú default.png) ---
-        public static Drivers Tasanapol_Inthraphuvasak = new Drivers { Name = "Tasanapol Inthraphuvasak", Number = 1, Team = "ART Grand Prix", Skill = 72, PhotoPath = "/Images/default.png" };
-        public static Drivers Kush_Maini = new Drivers { Name = "Kush Maini", Number = 4, Team = "ART Grand Prix", Skill = 77, PhotoPath = "/Images/default.png" };
-        public static Drivers Joshua_Duerksen = new Drivers { Name = "Joshua Dürksen", Number = 2, Team = "Invicta Racing", Skill = 75, PhotoPath = "/Images/default.png" };
-        public static Drivers Rafael_Camara = new Drivers { Name = "Rafael Câmara", Number = 5, Team = "Invicta Racing", Skill = 78, PhotoPath = "/Images/default.png" };
-        public static Drivers Oliver_Goethe = new Drivers { Name = "Oliver Goethe", Number = 3, Team = "MP Motorsport", Skill = 76, PhotoPath = "/Images/default.png" };
-        public static Drivers Gabriele_Mini = new Drivers { Name = "Gabriele Minì", Number = 21, Team = "MP Motorsport", Skill = 78, PhotoPath = "/Images/default.png" };
-        public static Drivers Nikola_Tsolov = new Drivers { Name = "Nikola Tsolov", Number = 6, Team = "Campos Racing", Skill = 74, PhotoPath = "/Images/default.png" };
-        public static Drivers Noel_Leon = new Drivers { Name = "Noel León", Number = 7, Team = "Campos Racing", Skill = 74, PhotoPath = "/Images/default.png" };
-        public static Drivers Alex_Dunne = new Drivers { Name = "Alex Dunne", Number = 8, Team = "Rodin Motorsport", Skill = 73, PhotoPath = "/Images/default.png" };
-        public static Drivers Martinius_Stenshorne = new Drivers { Name = "Martinius Stenshorne", Number = 9, Team = "Rodin Motorsport", Skill = 72, PhotoPath = "/Images/default.png" };
-        public static Drivers Dino_Beganovic = new Drivers { Name = "Dino Beganovic", Number = 10, Team = "DAMS Lucas Oil", Skill = 77, PhotoPath = "/Images/default.png" };
-        public static Drivers Roman_Bilinski = new Drivers { Name = "Roman Bilinski", Number = 12, Team = "DAMS Lucas Oil", Skill = 70, PhotoPath = "/Images/default.png" };
-        public static Drivers Laurens_Van_Hoepen = new Drivers { Name = "Laurens van Hoepen", Number = 11, Team = "Trident", Skill = 73, PhotoPath = "/Images/default.png" };
-        public static Drivers Nixon_Bennett = new Drivers { Name = "Nixon Bennett", Number = 17, Team = "Trident", Skill = 68, PhotoPath = "/Images/default.png" };
-        public static Drivers Ritomo_Miyata = new Drivers { Name = "Ritomo Miyata", Number = 13, Team = "Hitech", Skill = 75, PhotoPath = "/Images/default.png" };
-        public static Drivers Colton_Herta = new Drivers { Name = "Colton Herta", Number = 14, Team = "Hitech", Skill = 82, PhotoPath = "/Images/default.png" };
-        public static Drivers Sebastian_Montoya = new Drivers { Name = "Sebastian Montoya", Number = 15, Team = "Prema", Skill = 71, PhotoPath = "/Images/default.png" };
-        public static Drivers Mari_Boya = new Drivers { Name = "Mari Boya", Number = 22, Team = "Prema", Skill = 72, PhotoPath = "/Images/default.png" };
-        public static Drivers Rafael_Villagomez = new Drivers { Name = "Rafael Villagómez", Number = 16, Team = "Van Amersfoort Racing", Skill = 70, PhotoPath = "/Images/default.png" };
-        public static Drivers Nicolas_Varrone = new Drivers { Name = "Nicolás Varrone", Number = 19, Team = "Van Amersfoort Racing", Skill = 71, PhotoPath = "/Images/default.png" };
-        public static Drivers Emerson_Fittipaldi = new Drivers { Name = "Emerson Fittipaldi Jr.", Number = 18, Team = "AIX Racing", Skill = 76, PhotoPath = "/Images/default.png" };
-        public static Drivers Devlin_Shields = new Drivers { Name = "Devlin Shields", Number = 20, Team = "AIX Racing", Skill = 67, PhotoPath = "/Images/default.png" };
-
         public static List<Drivers> GetAllDrivers()
         {
-            return new List<Drivers>
-            {
+            List<Drivers> allDrivers = new List<Drivers>();
+
+            // Pole so všetkými jazdcami
+            Drivers[] array = new Drivers[] {
                 Max_Verstappen, Lando_Norris, Charles_Leclerc, Lewis_Hamilton, Oscar_Piastri,
                 Carlos_Sainz, George_Russell, Fernando_Alonso, Alex_Albon, Pierre_Gasly,
                 Valtteri_Bottas, Nico_Hulkenberg, Kimi_Antonelli, Esteban_Ocon, Liam_Lawson,
-                Franco_Colapinto, Sergio_Perez, Colton_Herta, Oliver_Bearman, Isack_Hadjar,
-                Gabriel_Bortoleto, Arvid_Lindblad, Lance_Stroll,
-                Tasanapol_Inthraphuvasak, Kush_Maini, Joshua_Duerksen, Rafael_Camara,
-                Oliver_Goethe, Gabriele_Mini, Nikola_Tsolov, Noel_Leon, Alex_Dunne,
-                Martinius_Stenshorne, Dino_Beganovic, Roman_Bilinski, Laurens_Van_Hoepen,
-                Nixon_Bennett, Ritomo_Miyata, Sebastian_Montoya, Mari_Boya,
-                Rafael_Villagomez, Nicolas_Varrone, Emerson_Fittipaldi, Devlin_Shields
+                Franco_Colapinto, Sergio_Perez, Oliver_Bearman, Isack_Hadjar,
+                Gabriel_Bortoleto, Arvid_Lindblad, Lance_Stroll
             };
+
+            // ZAČIATOČNÍCKY ŠTÝL: Použitie while cyklu na naplnenie listu
+            int index = 0;
+            while (index < array.Length)
+            {
+                allDrivers.Add(array[index]);
+                index = index + 1;
+            }
+
+            return allDrivers;
         }
     }
 }
--- F1 Manager 2026/Picking Team/Drivers_Pick.xaml.cs
+++ F1 Manager 2026/Picking Team/Drivers_Pick.xaml.cs
@@ -1,13 +1,111 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace F1_Manager_2026.Picking_Team
 {
+    /// <summary>
+    /// Logika pre okno Drivers_Pick.xaml
+    /// </summary>
     public partial class Drivers_Pick : Window
     {
         public Drivers_Pick()
         {
             InitializeComponent();
-            DriversItemsControl.ItemsSource = Drivers.GetAllDrivers();
+            LoadDataToUI();
+        }
+
+        private void LoadDataToUI()
+        {
+            // Načítanie všetkých jazdcov zo statickej triedy Drivers
+            List<Drivers> allDriversList = Drivers.GetAllDrivers();
+
+            if (allDriversList != null)
+            {
+                if (allDriversList.Count > 0)
+                {
+                    // Naplnenie ItemsControl dátami
+                    DriversItemsControl.ItemsSource = allDriversList;
+                }
+                else
+                {
+                    MessageBox.Show("Zoznam jazdcov je prázdny!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Dáta jazdcov sa nepodarilo načítať (null).");
+            }
+        }
+
+        // Táto metóda rieši Border pri vstupe myši
+        private void DriverCard_MouseEnter(object sender, MouseEventArgs e)
+        {
+            // Pretypujeme sender na Border, aby sme k nemu mali prístup
+            Border b = sender as Border;
+
+            if (b != null)
+            {
+                // Nastavenie červeného okraja a hrúbky 4
+                b.BorderBrush = Brushes.Red;
+                b.BorderThickness = new Thickness(4);
+
+                // Mierne zosvetlenie pozadia pre efekt "hover"
+                b.Background = new SolidColorBrush(Color.FromRgb(40, 40, 40));
+            }
+        }
+
+        // Táto metóda vráti Border do pôvodného stavu pri odchode myši
+        private void DriverCard_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Border b = sender as Border;
+
+            if (b != null)
+            {
+                // Návrat k pôvodnej šedej farbe (#333333) a hrúbke 2
+                b.BorderBrush = new SolidColorBrush(Color.FromRgb(51, 51, 51));
+                b.BorderThickness = new Thickness(2);
+
+                // Návrat k pôvodnému tmavému pozadiu (#1A1A1A)
+                b.Background = new SolidColorBrush(Color.FromRgb(26, 26, 26));
+            }
+        }
+        private void DriverCard_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // 1. Získame Border, na ktorý sa kliklo (Poradil Gemini) ---
+            Border kliknutyBorder = (Border)sender;
+
+            // 2. Vytiahneme si jazdca, ktorý patrí k tejto karte (vďaka Bindingu)
+            Drivers kliknutyJazdec = (Drivers)kliknutyBorder.DataContext;
+
+            // 3. Získame si zoznam všetkých jazdcov, aby sme v nich mohli hľadať
+            List<Drivers> vsetciJazdci = Drivers.GetAllDrivers();
+
+            // 4. Vytvoríme si prázdnu premennú, kam si uložíme výsledok
+            Drivers najdenyJazdec = null;
+
+            // 5. Prejdeme všetkých jazdcov jedného po druhom
+            foreach (Drivers j in vsetciJazdci)
+            {
+                // Hladame meno jazdca v liste podla zdroja Bindingu z borderu
+                if (j.Name == kliknutyJazdec.Name)
+                {
+                    PlayerTeam playerTeam = new PlayerTeam
+                    {
+                        driver1name = j.Name,
+                        driver1rating = j.Skill,
+                    };
+                    MessageBox.Show($"Vybral si jazdca: {playerTeam.driver1name} s ratingom {playerTeam.driver1rating}!");
+                }
+                else
+                {
+                    // Ak nie, pokračujeme v hľadaní ďalšieho jazdca
+                    continue;
+                }
+            }
         }
     }
 }
